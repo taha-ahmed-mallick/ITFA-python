ac1021d82eec61d90a1e12fbe003bbdc75df98cf
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-16 23:54:35 +0500

    done till 4

---
+++ lab-11/ex-2.py
@@ -0,0 +1,3 @@
+arr = [54, 43, 2, 1, 5]
+
+print("Length of list", len(arr))
---
+++ lab-11/ex-4.py
@@ -0,0 +1,4 @@
+sentence = input("Enter a sentence: ")
+
+if "Python" in sentence:
+    print("The sentence contains the word 'Python'.")
