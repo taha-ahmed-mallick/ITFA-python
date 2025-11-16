80fd1ab3a55663f9150aa4ac0a1615937c4d04dc
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2025-11-16 23:48:51 +0500

    1 done

---
+++ lab-11/ex-1.py
@@ -0,0 +1,12 @@
+a = int(input("a = "))
+b = int(input("b = "))
+op = int(input("operator = "))
+
+if op == 1:
+    print(a + b, end='')
+elif op == 2:
+    print(a - b, end='')
+elif op == 3:
+    print(a * b, end='')
+else:
+    print('"Invalid Input"', end='')
